dbf90f93d16170ad3a86d3cf2cab2b0ac03092bb
Kaio <kaio.santiago.13@hotmail.com>
2024-07-26 18:06:10 -0300

    first commit

---
+++ VariableServer/Controllers/VariableController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Mvc;
+using VariableServer.Models;
+
+namespace VariableServer.Controllers
+{
+    [Route("[controller]/")]
+    [ApiController]
+    public class VariableController : ControllerBase
+    {
+        private static Dictionary<string, object> _dictVariables;
+
+        public VariableController() 
+        { 
+            _dictVariables = Helper.Helper.LoadDictionaryFromFile(); 
+        }
+
+        #region GET
+        [HttpGet("GetVariables")]
+        public IActionResult GetVariables()
+        {
+            return Ok(_dictVariables);
+        }
+        [HttpGet("Get")]
+        public IActionResult Get(string key)
+        {
+            if (_dictVariables.Keys.Contains(key))
+            {
+                var value = _dictVariables[key];
+                var response = new
+                {
+                    key = key,
+                    value = value
+                };
+                return Ok(response);
+            }
+            else
+            {
+                return NotFound("");
+            }
+
+        }
+        #endregion
+
+        #region POST
+        [HttpPost("Set")]
+        public IActionResult Set([FromBody] Variable variable)
+        {
+            if (string.IsNullOrEmpty(variable.Key)) BadRequest("The params 'key' can't empty");
+            if (_dictVariables.Keys.Contains(variable.Key))
+            {
+                _dictVariables[variable.Key] = variable.Value;
+                Helper.Helper.SaveDictionaryToFile(_dictVariables);
+                return Ok("Update successful!");
+            }
+            else
+            {
+                _dictVariables.Add(variable.Key, variable.Value);
+                Helper.Helper.SaveDictionaryToFile(_dictVariables);
+                return Ok("Create successful!");
+            }
+        }
+        #endregion
+
+        #region DELETE
+        [HttpDelete("Delete")]
+        public IActionResult Delete(string key)
+        {
+            if (_dictVariables.Keys.Contains(key))
+            {
+                _dictVariables.Remove(key);
+                Helper.Helper.SaveDictionaryToFile(_dictVariables);
+                return Ok("Variable removed!");
+            }
+            else
+            {
+                return NotFound("Variable not found.");
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ VariableServer/Helper/Helper.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+
+namespace VariableServer.Helper
+{
+    public class Helper
+    {
+        private static readonly string _filePath = "C:\\temp\\data.json";
+        public static Dictionary<string, object>? _dictVariables;
+
+        /// <summary>
+        /// Save dictionary in a json file.
+        /// </summary>
+        /// /// <param name="_dictVariables">The key whose value needs to be retrieved.</param>
+        /// <returns></returns>
+        public static void SaveDictionaryToFile(Dictionary<string, object> _dictVariables)
+        {
+            var json = JsonSerializer.Serialize(_dictVariables);
+            System.IO.File.WriteAllText(_filePath, json);
+        }
+
+        /// <summary>
+        /// Load file and parse to a dictionary
+        /// </summary>
+        /// <returns></returns>
+        public static Dictionary<string, object> LoadDictionaryFromFile()
+        {
+            if (System.IO.File.Exists(_filePath))
+            {
+                if (!File.Exists(_filePath)) return new Dictionary<string, object>();
+                var json = System.IO.File.ReadAllText(_filePath);
+                _dictVariables = (JsonSerializer.Deserialize<Dictionary<string, object>>(json) == null? 
+                    new Dictionary<string, object>()
+                    : JsonSerializer.Deserialize<Dictionary<string, object>>(json));
+                return _dictVariables;
+            }
+            else
+            {
+                return new Dictionary<string, object>();
+            }           
+        }
+    }
+}
